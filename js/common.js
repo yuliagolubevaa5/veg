f7757e930ffda2ecf1300eacb309864edbec55ae
yuliagolubevaa5 <yuliagolubevaa5@gmail.com>
2019-12-10 17:14:25 +0300

    veg

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.main-slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1
+    });
+
+    $('.multiple-items').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1
+    });
+
+});
