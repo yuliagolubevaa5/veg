058e2c8b1799621061d3a36484b831f49a945e54
yuliagolubevaa5 <yuliagolubevaa5@gmail.com>
2019-12-20 07:57:08 +0300

    veg_four

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('.main-slider').slick({
         slidesToShow: 6,
         slidesToScroll: 1
@@ -9,4 +9,19 @@ $(document).ready(function () {
         slidesToScroll: 1
     });
 
+    $('.main-slider').find('.slick-active').last().addClass('slick-current');
+    $(".main-slider").on('afterChange', function() {
+        $('.main-slider').find('.slick-active').last().addClass('slick-current');
+    });
+
+    $('.multiple-items').find('.slick-active').last().addClass('slick-current');
+    $(".multiple-items").on('afterChange', function() {
+        $('.multiple-items').find('.slick-active').last().addClass('slick-current');
+    });
+
+
 });
+
+window.onload = function() {
+    jQuery("#user-city").text(ymaps.geolocation.city);
+}
