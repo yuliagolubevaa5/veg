1e2cead5a560cbc516f6eb63d0ac241aceef34c0
yuliagolubevaa5 <yuliagolubevaa5@gmail.com>
2020-01-14 14:59:49 +0300

    veg_six

--- js/common.js
+++ js/common.js
@@ -1,13 +1,64 @@
 $(document).ready(function() {
     $('.main-slider').slick({
         slidesToShow: 6,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        infinite: true,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 568,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     $('.multiple-items').slick({
         slidesToShow: 8,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 568,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
+
+
 
     $('.main-slider').find('.slick-active').last().addClass('slick-current');
     $(".main-slider").on('afterChange', function() {
